36e2470017d054100d6c18a040bd88bfa7cfe8f6
Vinh <vinh@listia.com>
2014-11-12 12:12:36 -0800

    Update MySQL to 5.6
    - We also introduce single package install mode with -p

--- bin/build.sh
+++ bin/build.sh
@@ -8,8 +8,14 @@ for MANIFEST in Manifest.*; do
 
   while read file; do
     printf "Including: $file\n"
-
-    cat "$file" >> "$FILENAME"
+    component=$(echo $file | awk -F'/' '{printf $2}')
+    if [[ "bash-shebang exit-trap check-home-bin header argument compiler-and-libraries mac-functions shared-functions start-services" =~ $component ]]; then
+      cat "$file" >> "$FILENAME"
+    else
+      echo "if ( echo \"$file\" | grep -q \"\$PACKAGE\" ); then" >> "$FILENAME"
+      cat "$file" >> "$FILENAME"
+      echo "fi" >> "$FILENAME"
+    fi
 
     printf "### end $file\n\n" >> "$FILENAME"
   done < "$MANIFEST"
